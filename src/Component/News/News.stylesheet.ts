d3560c262e07defb41f015f658cebc76396f60e4
Williams Bolade <dbolade2000@gmail.com>
2023-03-08 18:23:09 +0100

    Articles rendering

---
+++ src/Component/News/News.stylesheet.ts
@@ -0,0 +1,16 @@
+import {StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  newsBox: {
+    width: '90%',
+    marginTop: 10,
+    borderRadius: 7,
+    backgroundColor: 
+  },
+});
+
+export default styles;
