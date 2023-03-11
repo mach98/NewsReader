76d291148e9f338c4648970919849cecbf3da96c
Williams Bolade <dbolade2000@gmail.com>
2023-03-11 18:09:57 +0100

    News Details screen fixed

--- src/Screens/NewsScreen/NewsScreen.stylesheet.ts
+++ src/Screens/NewsScreen/NewsScreen.stylesheet.ts
@@ -1,5 +1,25 @@
-import {StyleSheet} from 'react-native/types';
+import {StyleSheet} from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    padding: 10,
+  },
+  newsHeader: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+    marginBottom: 20,
+    textAlign: 'center',
+    color: '#000',
+  },
+  newsContent: {
+    fontSize: 16,
+    textAlign: 'left',
+    color: '#000',
+  },
+});
 
 export default styles;
