8f0397b425dcc90eacaacd5fbf8cf88090a516e9
Williams Bolade <dbolade2000@gmail.com>
2023-03-23 09:30:25 +0100

    Icons added to bottom tab

--- App.tsx
+++ App.tsx
@@ -2,11 +2,13 @@ import {View, Text} from 'react-native';
 import 'react-native-gesture-handler';
 import {NavigationContainer} from '@react-navigation/native';
 import HomeStackNavigator from './src/Navigation/HomeStackNavigator';
+import BottomTabNavigator from './src/Navigation/BottomTabNavigator';
 
 function App() {
   return (
     <NavigationContainer>
-      <HomeStackNavigator />
+      {/* <HomeStackNavigator /> */}
+      <BottomTabNavigator />
     </NavigationContainer>
   );
 }
--- src/Navigation/BottomTabNavigator.tsx
+++ src/Navigation/BottomTabNavigator.tsx
@@ -4,6 +4,11 @@ import ExploreScreen from '../Screens/ExploreScreen/ExploreScreen';
 import HomeScreen from '../Screens/HomeScreen/HomeScreen';
 import SettingsScreen from '../Screens/SettingsScreen/SettingsScreen';
 import BookmarksScreen from '../Screens/BookmarksScreen/BookmarksScreen';
+import HomeIcon from 'react-native-vector-icons/Octicons';
+import BookmarkIcon from 'react-native-vector-icons/Octicons';
+import ExploreIcon from 'react-native-vector-icons/Feather';
+import SettingsIcon from 'react-native-vector-icons/Feather';
+import {ICON_SIZE} from '../Constants/dimensions';
 
 export type BottomTabParamList = {
   Home: undefined;
@@ -16,11 +21,41 @@ const Tab = createBottomTabNavigator<BottomTabParamList>();
 
 function BottomTabNavigator() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Bookmarks" component={BookmarksScreen} />
-      <Tab.Screen name="Explore" component={ExploreScreen} />
-      <Tab.Screen name="Settings" component={SettingsScreen} />
+    <Tab.Navigator screenOptions={{headerShown: false, tabBarShowLabel: false}}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({focused}) => <HomeIcon name="home" size={ICON_SIZE} />,
+        }}
+      />
+      <Tab.Screen
+        name="Bookmarks"
+        component={BookmarksScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <BookmarkIcon name="bookmark" size={ICON_SIZE} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Explore"
+        component={ExploreScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <ExploreIcon name="compass" size={ICON_SIZE} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Settings"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: ({focused}) => (
+            <SettingsIcon name="settings" size={ICON_SIZE} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
