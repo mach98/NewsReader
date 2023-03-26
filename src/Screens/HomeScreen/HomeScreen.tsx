b10d0ca814afd661b6d2f998dd1d7f1fe17b2f28
Williams Bolade <dbolade2000@gmail.com>
2023-03-26 16:35:15 +0100

    DrawrButton component created

--- src/Screens/HomeScreen/HomeScreen.tsx
+++ src/Screens/HomeScreen/HomeScreen.tsx
@@ -1,18 +1,17 @@
-import React, {FC} from 'react';
-import {Pressable, SafeAreaView, Text, View} from 'react-native';
+import React, {FC, useState} from 'react';
+import {SafeAreaView, Text, View} from 'react-native';
 import News from '../../Component/News/News';
 import styles from './HomeScreen.stylesheet';
 import HomeIcon from 'react-native-vector-icons/Ionicons';
-import DrawerIcon from 'react-native-vector-icons/Ionicons';
+import DrawerButton from '../../Component/DrawerButton/DrawerButton';
 
 const HomeScreen: FC = () => {
+  // const [toggle, setToggleDrawer] = useState(false);
+  const toggleDrawer = () => {};
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
-        <Pressable>
-          <DrawerIcon name="ios-grid-outline" size={20} />
-        </Pressable>
-
+        <DrawerButton onPress={toggleDrawer} />
         <View>
           <Text>Location</Text>
         </View>
