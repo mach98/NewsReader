8cbb7f73d9e82cfd29e7d27dd192343307255a98
Williams Bolade <dbolade2000@gmail.com>
2023-08-15 15:20:51 +0100

    UserAuthContext and App fixed

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import 'react-native-gesture-handler';
-<<<<<<< HEAD
+
 import {NavigationContainer} from '@react-navigation/native';
 import DrawerNavigator from './src/Navigation/DrawerNavigator';
 import {UserAuthProvider} from './src/Context/UserAuthContext';
@@ -13,12 +13,6 @@ function App() {
       </NavigationContainer>
     </UserAuthProvider>
   );
-=======
-import Providers from './src/Navigation';
-
-function App() {
-  return <Providers />;
->>>>>>> bf8616ccc4b7d645fb43f3b33eabcc57ae5b0d54
 }
 
 export default App;
